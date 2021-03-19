13c917c943e958998bd0464695742e09d6729cc6
Mostafa Mahmoud <mahmodmostafa028@gmail.com>
2021-03-20 00:47:20 +0200

    Handle user registrations

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :clients, controllers: { registrations: 'api/v1/clients/registrations' }
+  devise_for :users, controllers: { registrations: 'api/v1/users/registrations' }
   namespace :api do
     namespace :v1 do
       namespace :clients do
