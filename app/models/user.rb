8562cdee24a986eb796b5758acd9205dd99040bb
Mostafa Mahmoud <mahmodmostafa028@gmail.com>
2021-03-20 16:40:11 +0200

    Add code  property to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+
+  before_create :set_code
+  def set_code
+    self.code = DateTime.now.strftime('%s').concat(SecureRandom.hex(8))
+  end       
 end
