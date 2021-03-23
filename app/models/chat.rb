a43c08694abe8f232ead7969e05faf8756b02c2c
Mostafa Mahmoud <mahmodmostafa028@gmail.com>
2021-03-23 02:48:56 +0200

    Fix uniqueness validation chat name

--- app/models/chat.rb
+++ app/models/chat.rb
@@ -1,11 +1,16 @@
 class Chat < ApplicationRecord
-    belongs_to :application
-    has_and_belongs_to_many :users
-    before_create :set_chat_number
     validates :application_id,:name ,presence: true
-    validates :name, :uniqueness => true
+    validates :name, uniqueness: { scope: :application_id }, on: :create
+    validates :name, uniqueness: { scope: :application_id }, on: :update,if: :name_changed?
+
+    belongs_to :application
     has_many :messages
+    has_and_belongs_to_many :users
+
+    before_create :set_chat_number
+
     def set_chat_number
         self.chat_number= (self.application.chats.count)+1;
     end
+    
 end
