904fb7a72168c946e944ef34bfed79748d73b599
Mostafa Mahmoud <mahmodmostafa028@gmail.com>
2021-03-23 02:47:36 +0200

    Fix uniqueness  validation on application name

--- app/models/application.rb
+++ app/models/application.rb
@@ -2,7 +2,8 @@ class Application < ApplicationRecord
   belongs_to :client
   has_many :chats
   validates :name, presence: true
-  validates :name, :uniqueness => true
+  validates :name, uniqueness: true, on: :create
+  validates :name, uniqueness: true, on: :update,if: :name_changed?
   before_create :set_token
   def set_token
     self.token = DateTime.now.strftime('%s').concat(SecureRandom.hex)
